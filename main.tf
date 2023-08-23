3e6be8f5fbd64a7b7717a80dcfe5527765796c4e
EPAPRA <dave.devops109@gmail.com>
2023-08-23 14:47:14 +0530

    multibranch commit

--- main.tf
+++ main.tf
@@ -41,8 +41,10 @@ resource "aws_docdb_cluster" "docdb" {
   master_username         = data.aws_ssm_parameter.DB_ADMIN_USER.value
   master_password         = data.aws_ssm_parameter.DB_ADMIN_PASS.value
   skip_final_snapshot     = true
-  db_subnet_group_name = aws_docdb_subnet_group.default.name
-  vpc_security_group_ids = [aws_security_group.docdb.id]
+  db_subnet_group_name    = aws_docdb_subnet_group.default.name
+  vpc_security_group_ids  = [aws_security_group.docdb.id]
+  storage_encrypted = true
+  kms_key_id = data.aws_kms_key.key.arn
 
   tags = merge(
     local.common_tags,
@@ -55,6 +57,8 @@ resource "aws_docdb_cluster_instance" "cluster_instances" {
   identifier         = "${var.env}-docdb-cluster-instance-${count.index+1}"
   cluster_identifier = aws_docdb_cluster.docdb.id
   instance_class     = var.instance_class
+  storage_encrypted = true
+  kms_key_id = data.aws_kms_key.key.arn
   tags = merge(
     local.common_tags,
     { Name = "${var.env}-docdb-cluster-instance-${count.index+1}" }
